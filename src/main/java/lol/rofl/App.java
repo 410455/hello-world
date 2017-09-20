5c2bcc97864c11859434b24cf31a4c47d6ce8d04
Jan Kalfus <410455@mail.muni.cz>
2017-09-20 12:30:59 +0200

    Initial commit

---
+++ src/main/java/lol/rofl/App.java
@@ -0,0 +1,13 @@
+package lol.rofl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
